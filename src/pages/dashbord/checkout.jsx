d2ce55dce664874b223fb3d30afa1cb2d64d2f08
rizveejack <jrizvee@gmail.com>
2021-11-10 21:17:14 +0600

    change env key

--- src/pages/dashbord/checkout.jsx
+++ src/pages/dashbord/checkout.jsx
@@ -5,7 +5,7 @@ import CheckoutComponent from "../../components/shopComponent/CheckoutComponent"
 import FrontEnd from "../../layout/FrontEnd"
 
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_WORDPRESS)
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PK)
 
 const Checkout = () => {
     return (
