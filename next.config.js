31752b7e46085f5626ead73fac71001235c2427d
rizveejack <jrizvee@gmail.com>
2021-11-10 16:46:37 +0600

    update to next 12

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const allowedImageWordPressDomain = new URL(process.env.NEXT_PUBLIC_WORDPRESS_URL).hostname
 module.exports = {
   reactStrictMode: true,
+  swcMinify: true,
   images:{
     domains: [`${allowedImageWordPressDomain}`]
   }
